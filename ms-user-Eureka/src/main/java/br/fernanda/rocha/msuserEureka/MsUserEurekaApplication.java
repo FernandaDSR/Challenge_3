f67fd3250433f642d4293d2019afcbbde3288e8c
Fernanda <fernanda.dsr96@gmail.com>
2023-10-19 20:55:53 -0300

    ga

---
+++ ms-user-Eureka/src/main/java/br/fernanda/rocha/msuserEureka/MsUserEurekaApplication.java
@@ -0,0 +1,15 @@
+package br.fernanda.rocha.msuserEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsUserEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserEurekaApplication.class, args);
+	}
+
+}
---
+++ ms-user-Eureka/src/test/java/br/fernanda/rocha/msuserEureka/MsUserEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fernanda.rocha.msuserEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
